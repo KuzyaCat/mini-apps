1d5722aac246b213d07cf131c7764fb60c1d0e48
KuzyaCat <lizerd34@mail.ru>
2021-08-10 18:01:35 +0300

    Add Command

---
+++ design-patterns/js/behavioral/command/command.js
@@ -0,0 +1,52 @@
+// Separate the customer from the recipient
+
+class Cockpit {
+  constructor(command) {
+    this.command = command;
+  }
+
+  execute() {
+    this.command.execute();
+  }
+}
+
+class Turbine {
+  constructor() {
+    this.state = false;
+  }
+
+  on() {
+    this.state = true;
+  }
+
+  off() {
+    this.state = false;
+  }
+}
+
+class OnCommand {
+  constructor(turbine) {
+    this.turbine = turbine;
+  }
+
+  execute() {
+    this.turbine.on();
+  }
+}
+
+class OffCommand {
+  constructor(turbine) {
+    this.turbine = turbine;
+  }
+
+  execute() {
+    this.turbine.off();
+  }
+}
+
+export {
+  Cockpit,
+  Turbine,
+  OnCommand,
+  OffCommand
+};
