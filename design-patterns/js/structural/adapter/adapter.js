254a101c2f77f4697e46699d01f8fb79f2e6d05e
KuzyaCat <lizerd34@mail.ru>
2021-08-10 14:04:36 +0300

    Add adapter

---
+++ design-patterns/js/structural/adapter/adapter.js
@@ -0,0 +1,37 @@
+// Wraps an incompatible object and makes it compatible without changing their code
+
+class Soldier {
+  constructor(level) {
+    this.level = level;
+  }
+
+  attack() {
+    return this.level * 1;
+  }
+}
+
+class Jedi {
+  constructor(level) {
+    this.level = level;
+  }
+
+  attackWithSaber() {
+    return this.level * 100;
+  }
+}
+
+class JediAdapter {
+  constructor(jedi) {
+    this.jedi = jedi;
+  }
+
+  attack() {
+    return this.jedi.attackWithSaber();
+  }
+}
+
+export {
+  Soldier,
+  Jedi,
+  JediAdapter
+};
