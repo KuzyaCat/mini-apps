e631e751bc3754a855ececb625b99b55cd1aad25
KuzyaCat <lizerd34@mail.ru>
2021-08-10 17:47:54 +0300

    Add Chain Of Responsibility

---
+++ design-patterns/js/behavioral/chain-of-responsibility/chain-of-responsibility.js
@@ -0,0 +1,73 @@
+// Allows you to transfer requests sequentially along the chain of handlers,
+// and each subsequent handler decides whether it can process it itself,
+// or pass it further along the chain
+
+class ShoppingCart {
+  constructor() {
+    this.products = [];
+  }
+
+  addProduct(p) {
+    this.products.push(p);
+  };
+}
+
+class Discount {
+  calc(products) {
+    let ndiscount = new NumberDiscount();
+    let pdiscount = new PriceDiscount();
+    let none = new NoneDiscount();
+    ndiscount.setNext(pdiscount);
+    pdiscount.setNext(none);
+    return ndiscount.exec(products);
+  };
+}
+
+class NumberDiscount {
+  constructor() {
+    this.next = null;
+  }
+
+  setNext(fn) {
+    this.next = fn;
+  };
+
+  exec(products) {
+    let result = 0;
+    if (products.length > 3)
+      result = 0.05;
+
+    return result + this.next.exec(products);
+  };
+}
+
+class PriceDiscount {
+  constructor() {
+    this.next = null;
+  }
+
+  setNext(fn) {
+    this.next = fn;
+  };
+
+  exec(products) {
+    let result = 0;
+    let total = products.reduce((a, b) => a + b);
+
+    if (total >= 500)
+      result = 0.1;
+
+    return result + this.next.exec(products);
+  };
+}
+
+class NoneDiscount {
+  exec() {
+    return 0;
+  };
+}
+
+export {
+  ShoppingCart,
+  Discount,
+}
