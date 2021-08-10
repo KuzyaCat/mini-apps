27769a76aa39bb188eeccf403ceea9a26e6a9c15
KuzyaCat <lizerd34@mail.ru>
2021-08-10 14:01:07 +0300

    Add adapter

---
+++ design-patterns/js/structural/proxy/proxy.js
@@ -0,0 +1,28 @@
+// Layer before the call
+
+class Car {
+  drive() {
+    return "driving";
+  };
+}
+
+class CarProxy {
+  constructor(driver) {
+    this.driver = driver;
+  }
+  drive() {
+    return (this.driver.age < 18) ? "too young to drive" : new Car().drive();
+  };
+}
+
+class Driver {
+  constructor(age) {
+    this.age = age;
+  }
+}
+
+export {
+  Car,
+  CarProxy,
+  Driver
+};
