ef8b58c17955f19795b2bb18790c061bd0053bc2
KuzyaCat <lizerd34@mail.ru>
2021-08-10 17:53:02 +0300

    Add memento

---
+++ design-patterns/js/behavioral/memento/memento.js
@@ -0,0 +1,36 @@
+// Allows you to save and restore the previous state of the object
+
+class Memento {
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+const originator = {
+  store: function(val) {
+    return new Memento(val);
+  },
+
+  restore: function(memento) {
+    return memento.value;
+  }
+};
+
+class Caretaker {
+  constructor() {
+    this.values = [];
+  }
+
+  addMemento(memento) {
+    this.values.push(memento);
+  }
+
+  getMemento(index) {
+    return this.values[index];
+  }
+}
+
+export {
+  originator,
+  Caretaker
+};
